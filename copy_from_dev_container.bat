e38b1c56f51ce08f0a217dbb6ea90f4fd713d8e1
Matt Lyle <matthewlyle@gmail.com>
2024-07-26 19:32:42 -0400

    helper script

---
+++ copy_from_dev_container.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo Using container: %1
+
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca_status/wakespca.py wake_spca_status/
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca_status/__init__.py wake_spca_status/
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca_status/config_flow.py wake_spca_status/
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca_status/const.py wake_spca_status/
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca_status/manifest.json wake_spca_status/
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca_status/sensor.py wake_spca_status/
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca_status/strings.json wake_spca_status/
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca_status/translations/en.json wake_spca_status/translations/en.json
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca_status/wakespca.py wake_spca_status/
