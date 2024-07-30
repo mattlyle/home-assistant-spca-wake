eccbeb8dfc2ae0314a3ba3beea8d83a6bedaea30
Matt Lyle <matthewlyle@gmail.com>
2024-07-29 21:49:23 -0400

    file permissions need set after copy

--- copy_to_dev_container.bat
+++ copy_to_dev_container.bat
@@ -11,3 +11,7 @@ docker cp wake_spca/manifest.json %1:/workspaces/homeassistant-core/homeassistan
 docker cp wake_spca/sensor.py %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
 docker cp wake_spca/strings.json %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
 docker cp wake_spca/translations/en.json %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/translations/
+
+docker exec -it -u root %1 chown vscode:vscode -R /workspaces/homeassistant-core/homeassistant/components/wake_spca
+
+docker exec -it %1 ls -lah /workspaces/homeassistant-core/homeassistant/components/wake_spca
