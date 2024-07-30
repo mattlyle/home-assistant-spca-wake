f0c14f0ad45e3a573dac811d7c067f0aae78874e
Matt Lyle <matthewlyle@gmail.com>
2024-07-29 21:04:53 -0400

    more rename

--- copy_to_dev_container.bat
+++ copy_to_dev_container.bat
@@ -10,4 +10,4 @@ docker cp wake_spca/const.py %1:/workspaces/homeassistant-core/homeassistant/com
 docker cp wake_spca/manifest.json %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
 docker cp wake_spca/sensor.py %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
 docker cp wake_spca/strings.json %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
-docker cp wake_spca/translations/en.json %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/ wake_spca/translations/
+docker cp wake_spca/translations/en.json %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/translations/
