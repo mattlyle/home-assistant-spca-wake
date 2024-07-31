d00e1f96225750925deea5be142315482d318f86
Matt Lyle <matthewlyle@gmail.com>
2024-07-30 21:44:33 -0400

    getting ready for hacs

--- copy_from_dev_container.bat
+++ copy_from_dev_container.bat
@@ -2,12 +2,12 @@
 
 echo Using container: %1
 
-docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/wake_spca.py wake_spca/
-docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/__init__.py wake_spca/
-docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/config_flow.py wake_spca/
-docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/coordinator.py wake_spca/
-docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/const.py wake_spca/
-docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/manifest.json wake_spca/
-docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/sensor.py wake_spca/
-docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/strings.json wake_spca/
-docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/translations/en.json wake_spca/translations/en.json
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/wake_spca.py custom_components/wake_spca/
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/__init__.py custom_components/wake_spca/
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/config_flow.py custom_components/wake_spca/
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/coordinator.py custom_components/wake_spca/
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/const.py custom_components/wake_spca/
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/manifest.json custom_components/wake_spca/
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/sensor.py custom_components/wake_spca/
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/strings.json custom_components/wake_spca/
+docker cp %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/translations/en.json custom_components/wake_spca/translations/en.json
--- copy_to_dev_container.bat
+++ copy_to_dev_container.bat
@@ -2,15 +2,15 @@
 
 echo Using container: %1
 
-docker cp wake_spca/wake_spca.py %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
-docker cp wake_spca/__init__.py %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
-docker cp wake_spca/config_flow.py %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
-docker cp wake_spca/coordinator.py %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
-docker cp wake_spca/const.py %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
-docker cp wake_spca/manifest.json %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
-docker cp wake_spca/sensor.py %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
-docker cp wake_spca/strings.json %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
-docker cp wake_spca/translations/en.json %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/translations/
+docker cp custom_components/wake_spca/wake_spca.py %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
+docker cp custom_components/wake_spca/__init__.py %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
+docker cp custom_components/wake_spca/config_flow.py %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
+docker cp custom_components/wake_spca/coordinator.py %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
+docker cp custom_components/wake_spca/const.py %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
+docker cp custom_components/wake_spca/manifest.json %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
+docker cp custom_components/wake_spca/sensor.py %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
+docker cp custom_components/wake_spca/strings.json %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/
+docker cp custom_components/wake_spca/translations/en.json %1:/workspaces/homeassistant-core/homeassistant/components/wake_spca/translations/
 
 docker exec -it -u root %1 chown vscode:vscode -R /workspaces/homeassistant-core/homeassistant/components/wake_spca
 
